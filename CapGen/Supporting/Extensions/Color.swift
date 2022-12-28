0c1df8759bd505a20cb17d6250e9d71c7f5106bf
Kevin Vu <kevintv789@gmail.com>
2022-12-28 17:56:38 -0500

    - Added initial design for the button pills - Added logic for social media platform selection - Added initial style for background - Added App icon - Set up color schemes - Set up custom fonts

---
+++ CapGen/Supporting/Extensions/Color.swift
@@ -0,0 +1,28 @@
+//
+//  Color.swift
+//  CapGen
+//
+//  Created by Kevin Vu on 12/27/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let ui = Color.UI()
+    
+    struct UI {
+        let middleYellowRed = Color("Middle Yellow Red")
+        let middleBluePurple = Color("Middle Blue Purple")
+        let lavenderBlue = Color("Lavender Blue")
+        let cadetBlueCrayola = Color("Cadet Blue Crayola")
+        let pink = Color("Orchid Crayola")
+        let darkSalmon = Color("Dark Salmon")
+        let frenchBlueSky = Color("French Blue Sky")
+        let lightCyan = Color("Light Cyan")
+        let richBlack = Color("Rich Black")
+        let cultured = Color("Cultured")
+        let lightOldPaper = Color("Light Old Paper")
+        let lighterLavBlue = Color("Lighter Lavender Blue")
+        let shadowGray = Color("Shadow Gray")
+    }
+}
