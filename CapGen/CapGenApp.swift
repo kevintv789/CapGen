b0fe9208d54677a03eff398b2d7954925af21712
Kevin Vu <kevintv789@gmail.com>
2022-12-27 21:26:45 -0500

    Initial Commit

---
+++ CapGen/CapGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapGenApp.swift
+//  CapGen
+//
+//  Created by Kevin Vu on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct CapGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
