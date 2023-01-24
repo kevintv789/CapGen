2528b3dfe5288dc191963784c6517da9007028ea
Kevin Vu <kevintv789@gmail.com>
2023-01-24 12:48:23 -0500

    Created an informational pop-up to explain the caption options within caption view

--- CapGen/Views/Caption/PopulatedCaptionsView.swift
+++ CapGen/Views/Caption/PopulatedCaptionsView.swift
@@ -313,30 +313,43 @@ struct ConfigurationIndicatorsView: View {
             CircularView(image: element.includeHashtags ? "yes-hashtag" : "no-hashtag")
             
             // Caption length
-            CircularView(image: element.captionLength, imageWidth: 15)
+            CircularView(image: element.captionLength, imageWidth: 20)
         }
         
     }
 }
 
+enum CircularViewSize {
+    case large, regular
+}
+
 struct CircularTonesView: View {
     let tones: [ToneModel]
+    @State var circularViewSize: CircularViewSize = .regular
+    
+    private func calculateSize() -> CGFloat {
+        if circularViewSize == .regular {
+            return 35.0
+        }
+        
+        return 50.0
+    }
     
     var body: some View {
         ZStack {
             Circle()
                 .fill(Color.ui.cultured)
-                .frame(width: 35, height: 35)
+                .frame(width: calculateSize(), height: calculateSize())
             
             // If there are 2 tones for a caption group, then group them together in one circle
             if (!tones.isEmpty) {
                 if (tones.count > 1) {
                     Text(tones[0].icon)
-                        .font(.ui.headlineSm)
+                        .font(circularViewSize == .regular ? .ui.headlineSm : Font.ui.headline)
                         .offset(x: -5, y: -5)
                     
                     Text(tones[1].icon)
-                        .font(.ui.headlineSm)
+                        .font(circularViewSize == .regular ? .ui.headlineSm : Font.ui.headline)
                         .offset(x: 5, y: 3)
                 } else {
                     Text(tones[0].icon)
@@ -349,18 +362,27 @@ struct CircularTonesView: View {
 
 struct CircularView: View {
     let image: String
-    @State var imageWidth: CGFloat = 20
+    @State var imageWidth: CGFloat?
+    @State var circularViewSize: CircularViewSize = .regular
+    
+    private func calculateSize() -> CGFloat {
+        if circularViewSize == .regular {
+            return 35.0
+        }
+        
+        return 50.0
+    }
     
     var body: some View {
         // Emojis
         ZStack {
             Circle()
                 .fill(Color.ui.cultured)
-                .frame(width: 35, height: 35)
+                .frame(width: calculateSize(), height: calculateSize())
             
             Image(image)
                 .resizable()
-                .frame(width: imageWidth, height: 20)
+                .frame(width: calculateSize() - (imageWidth != nil ? imageWidth! : 15), height: calculateSize() - 15)
         }
     }
 }
