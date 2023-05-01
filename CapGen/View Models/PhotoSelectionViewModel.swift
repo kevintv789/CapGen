5af636f043606a45905f435ee3e906910846198e
Kevin Vu <kevintv789@gmail.com>
2023-05-01 15:29:30 -0400

    added error handling for images

--- CapGen/View Models/PhotoSelectionViewModel.swift
+++ CapGen/View Models/PhotoSelectionViewModel.swift
@@ -14,32 +14,85 @@ import PhotosUI
 import SwiftUI
 import SwiftyJSON
 
+enum GoogleCloudVisionError: LocalizedError, Identifiable {
+    case imageSizeExceeded
+    case unsupportedImageFormat
+    case encodingError
+    case otherError(String)
+
+    var errorDescription: String? {
+        switch self {
+        case .imageSizeExceeded:
+            return "The selected image is too large. Please select an image smaller than 4MB."
+        case .unsupportedImageFormat:
+            return "The selected image format is not supported. Please select a JPEG, PNG, GIF, BMP, or WEBP image."
+        case .encodingError:
+            return "There was an error encoding the image. Please try again with a different image."
+        case .otherError(let message):
+            return message
+        }
+    }
+    
+    var id: String {
+        switch self {
+        case .imageSizeExceeded:
+            return "imageSizeExceeded"
+        case .unsupportedImageFormat:
+            return "unsupportedImageFormat"
+        case .encodingError:
+            return "encodingError"
+        case .otherError(let errorMessage):
+            return errorMessage
+        }
+    }
+}
+
 class PhotoSelectionViewModel: ObservableObject {
     @Published var photosPickerData: Data? = nil
     @Published var imageAddress: ImageGeoLocationAddress? = nil
     @Published var visionData: ParsedGoogleVisionImageData? = nil
+    @Published var googleCloudVisionError: GoogleCloudVisionError?
 
     func resetPhotoSelection() {
         photosPickerData = nil
         imageAddress = nil
         visionData = nil
     }
-
+    
     func assignPhotoPickerItem(image: PhotosPickerItem) async {
         if let data = try? await image.loadTransferable(type: Data.self) {
             DispatchQueue.main.async {
+                // Reset the error property before processing the image
+                self.googleCloudVisionError = nil
+                
+                // Check if the image format is supported
+                guard self.isImageFormatSupported(imageData: data) else {
+                    self.googleCloudVisionError = .unsupportedImageFormat
+                    return
+                }
+                
+                // Check if the image size is small enough
+                if data.count > 20 * 1024 * 1024 {
+                    self.googleCloudVisionError = .imageSizeExceeded
+                    return
+                }
+                
+                
+                
                 self.photosPickerData = data
-
+                
                 // Fetch image metadata and get geolocation
                 self.fetchImageMetadata(imageData: self.photosPickerData)
             }
         }
     }
-
+    
     // Use Google Cloud Vision API to analyze image
     func analyzeImage(image: UIImage, apiKey: String) async throws -> JSON {
         // Encode the image to base64
         guard let base64Image = image.jpegData(compressionQuality: 0.8)?.base64EncodedString() else {
+            // Check for encoding errors
+            self.googleCloudVisionError = .encodingError
             throw NSError(domain: "Image encoding error", code: -1, userInfo: nil)
         }
 
@@ -113,6 +166,26 @@ class PhotoSelectionViewModel: ObservableObject {
                 self.imageAddress = result
             }
         }
+    }
+    
+    /**
+     This function checks if the image format of the given imageData is supported by examining the first byte of the imageData. The first byte, also known as the magic number or signature, can be used to identify common image formats.
+
+     - Parameter imageData: The image data to be checked for supported format.
+     
+     - Returns: A boolean value indicating whether the image format is supported or not.
+
+     Supported image formats:
+     - 0xFF: JPEG (Joint Photographic Experts Group) format
+     - 0x89: PNG (Portable Network Graphics) format
+     - 0x47: GIF (Graphics Interchange Format) 87a or 89a format
+     - 0x42: BMP (Windows Bitmap) format
+     - 0x52: WebP format
+    */
+    func isImageFormatSupported(imageData: Data) -> Bool {
+        let supportedFormats: [UInt8] = [0xFF, 0x89, 0x47, 0x42, 0x52]
+        let firstByte = imageData[0]
+        return supportedFormats.contains(firstByte)
     }
 
     // This function filters any text annotation to only real world words
